e2675ea4b02049dbf3fc8d8abc291d7f54b358e8
nadia.domagala <nadia.domagala@onet.pl>
2023-10-08 17:43:04 +0200

    Tescik

---
+++ Testowe.cpp
@@ -0,0 +1,10 @@
+﻿// Testowe.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
